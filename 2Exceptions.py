08b1e6e81548ef53db8ee87ef66fd391a987a754
tphung-alt <t.phung@my.metroed.net>
2026-01-27 20:13:59 -0800

    Add 2Exceptions.py with error tracebacks

---
+++ 2Exceptions.py
@@ -0,0 +1,15 @@
+10 * (1/10)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    10 * (1/10)
+#-----------------------
+spam=76
+4 + spam*3
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    4 + spam*3
+#-------------------------------
+'2' + str(2)
+Traceback (most recent call last):
+  File "<stdin>", line 1, in <module>
+    '2' + str(2)
